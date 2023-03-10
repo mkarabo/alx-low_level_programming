a3dd806f29393fa93cd3b4a4c92841e1ddab1014
mkarabo <mkarabo7@gmail.com>
2023-03-10 11:45:46 -0800

    create static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+void _puts(char *str);
+/**
+ * main - Entry point
+ * _puts - prints
+void _puts(char *str) * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
